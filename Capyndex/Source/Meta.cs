16a030d7b322a8158d1300ee374a50ae8a7358f1
jludvik <jiri.ludvik@techfides.cz>
2025-04-13 19:49:40 +0200

    initial template commit

---
+++ Capyndex/Source/Meta.cs
@@ -0,0 +1,7 @@
+﻿global using FastEndpoints;
+global using FastEndpoints.Security;
+global using FastEndpoints.Swagger;
+global using Capyndex;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Tests")]
