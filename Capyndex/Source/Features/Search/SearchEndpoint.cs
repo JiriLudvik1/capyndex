ad51b9d1ffc88378804f5589647d5ff80606c047
jludvik <jiri.ludvik@techfides.cz>
2025-04-14 10:51:28 +0200

    implemented multiple words endpoint

---
+++ Capyndex/Source/Features/Search/SearchEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using Capyndex.Services;
+
+namespace Capyndex.Features.Search;
+
+public sealed record SearchRequest(string Query);
+
+public sealed record SearchResponse(Guid[] Results);
+
+public class SearchEndpoint(RedisIndexService redisIndexService) : Endpoint<SearchRequest, SearchResponse>
+{
+    public override void Configure()
+    {
+        Post("/search");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(SearchRequest request, CancellationToken ct)
+    {
+        var result = await redisIndexService.GetFullTextSearchAsync(query: request.Query);
+        await SendAsync(new(result), cancellation: ct);
+    }
+}
--- Capyndex/Source/Services/RedisIndexService.cs
+++ Capyndex/Source/Services/RedisIndexService.cs
@@ -34,6 +34,51 @@ public class RedisIndexService(IConnectionMultiplexer redis)
                .ToList();
     }
 
+    public async Task<Guid[]> GetFullTextSearchAsync(string query)
+    {
+        var terms = Tokenizer.Tokenize(query).ToList();
+
+        if (!terms.Any())
+        {
+            return [];
+        }
+
+        Dictionary<Guid, int> documentTermCounts = new();
+
+        foreach (var term in terms)
+        {
+            var key = $"index:{term}";
+            var entries = await _db.HashGetAllAsync(key);
+
+            if (term == terms.First())
+            {
+                foreach (var entry in entries)
+                {
+                    var docId = Guid.Parse(entry.Name!);
+                    documentTermCounts[docId] = 1;
+                }
+
+                continue;
+            }
+
+            foreach (var entry in entries)
+            {
+                var docId = Guid.Parse(entry.Name!);
+
+                if (documentTermCounts.TryGetValue(docId, out var value))
+                {
+                    documentTermCounts[docId] = ++value;
+                }
+            }
+        }
+
+        // Return only document IDs that contain all terms (count equals the number of terms)
+        return documentTermCounts
+               .Where(kvp => kvp.Value == terms.Count)
+               .Select(kvp => kvp.Key)
+               .ToArray();
+    }
+
     public async Task<bool> IsEmptyAsync()
     {
         var server = _db.Multiplexer.GetServer(_db.Multiplexer.GetEndPoints().First());
